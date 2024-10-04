725f6a64f8f9b702ae3f87f85dfbc615542436f7
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-03 21:11:38 -0500

    Remove # in gridContainer variable

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const gridContainer = document.getElementById("#container");
+    const gridContainer = document.getElementById("container");
     for (let i = 0; i < 256; i++) {
         // loop will run 256x as 16 * 16 = 256
         const squareDiv = document.createElement("div");
