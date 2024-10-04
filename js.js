9fd8ff7ec18805ca864ef02bf0e08a37307c8eef
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-03 21:18:30 -0500

    Add js comment for domcontentloaded EL

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+// Executes script after content is loaded on webpage
     const gridContainer = document.getElementById("container");
     for (let i = 0; i < 256; i++) {
         // loop will run 256x as 16 * 16 = 256
