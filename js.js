f577508863095501b7b643a6a9c497e5afb91f22
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-05 15:52:49 -0500

    Create gridSize() with size parameter outside loop'

--- js.js
+++ js.js
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 // Executes script after content is loaded on webpage
     const gridContainer = document.getElementById("container");
-    for (let i = 0; i < 256; i++) {
-        // loop will run 256x as 16 * 16 = 256
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add('squareDiv');
-        gridContainer.appendChild(squareDiv);
 
-        squareDiv.addEventListener('mouseover', () => {
-            function randomColor() {
-                let letters = '0123456789ABCDEF';
-                let color = '#'
-                for (let i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
+    function gridSize = (size) {
+        for (let i = 0; i < 256; i++) {
+            // loop will run 256x as 16 * 16 = 256
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add('squareDiv');
+            gridContainer.appendChild(squareDiv);
+
+            squareDiv.addEventListener('mouseover', () => {
+                function randomColor() {
+                    let letters = '0123456789ABCDEF';
+                    let color = '#'
+                    for (let i = 0; i < 6; i++) {
+                        color += letters[Math.floor(Math.random() * 16)];
+                    }
+                    return color;
                 }
-                return color;
-            }
-            squareDiv.style.backgroundColor = randomColor();
-        })
-        
-    }  
-
+                squareDiv.style.backgroundColor = randomColor();
+            });
+            
+        }; 
+    };
 });
 
