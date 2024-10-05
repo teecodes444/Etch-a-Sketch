a778bce06a8c9cd279a09a1d619ca33402cbc0da
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-04 19:36:35 -0500

    Create for loop to generate random color code

--- js.js
+++ js.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
         squareDiv.addEventListener('mouseout', () => {
             let letters = '0123456789ABCDEF';
             let color = '#'
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
             squareDiv.style.backgroundColor = 'blue';
         })
     }
